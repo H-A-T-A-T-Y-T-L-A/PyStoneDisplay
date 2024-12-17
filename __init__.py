d327cc4ff0a5d6ab62534eaaeec289fa1b07e5c3
hatatytla <jakub.franka@gmail.com>
2024-12-17 11:22:45 +0100

    add stone slide view

--- __init__.py
+++ __init__.py
@@ -9,4 +9,5 @@ from .stone_widget import StoneWidget
 from .stone_window import StoneWindow
 from .stone_image import StoneImage
 from .stone_label import StoneLabel
+from .stone_slide_view import StoneSlideView
 from .stone_display import StoneDisplay
---
+++ stone_slide_view.py
@@ -0,0 +1,53 @@
+from typing import Optional
+from . import StoneWidget, StoneWidgetCommandType
+
+class StoneSlideViewPage(StoneWidget):
+
+    def __init__(self, name: str, parent:Optional[StoneWidget] = None) -> None:
+        super().__init__(name, parent)
+
+class StoneSlideView(StoneWidget):
+
+    type_name = 'label'
+
+    def __init__(self, name:str, parent:Optional[StoneWidget] = None) -> None:
+        super().__init__(name, parent)
+        self._index = 0
+        self.set_view = StoneWidgetCommandType('set_view', StoneSlideView)
+        self._auto_play:int = 0
+        self.set_auto_play = StoneWidgetCommandType('set_auto_play', StoneSlideView)
+
+    def add_page(self, name:str) -> StoneSlideViewPage:
+        return StoneSlideViewPage(name, self)
+
+    def add_child(self, child: StoneWidget) -> None:
+        if not isinstance(child, StoneSlideViewPage):
+            raise TypeError(f'{StoneSlideView.__name__} expects children of type {StoneSlideViewPage.__name__}, not {type(child).__name__}')
+        return super().add_child(child)
+
+    @property
+    def current_index(self) -> int:
+        return self._index
+
+    @current_index.setter
+    def current_index(self, value:int) -> None:
+        if value >= len(self.children):
+            raise IndexError(f'Index "{value}" outside of allowed range <0, {len(self.children) - 1}>')
+        self._index = value
+        self.push_command(self.set_view, index = self._index)
+
+    @property
+    def current_page(self) -> StoneSlideViewPage:
+        page = self.children[self.current_index]
+        if not isinstance(page, StoneSlideViewPage):
+            raise TypeError(f'{StoneSlideView.__name__} expects children of type {StoneSlideViewPage.__name__}, not {type(page).__name__}')
+        return page
+
+    @property
+    def auto_play(self) -> int:
+        return self._auto_play
+
+    @auto_play.setter
+    def auto_play(self, value:int) -> None:
+        self._auto_play = value
+        self.push_command(self.set_auto_play, auto_play = value)
